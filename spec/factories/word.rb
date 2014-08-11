5d19cb3a251e15626e8c85ba596d3b8256036540
max-si-m <mak7.dj@gmail.com>
2014-08-12 01:16:24 +0300

    fix Word model

--- spec/factories/word.rb
+++ spec/factories/word.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :word, :class => 'Words' do
+  factory :word, :class => 'Word' do
     original "MyString"
     translit "MyString"
   end
