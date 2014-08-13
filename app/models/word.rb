80bc57690d43eb9d7d03468417324e28790328e4
max-si-m <mak7.dj@gmail.com>
2014-08-14 02:45:05 +0300

    fix test and add todo note.. 02:44 =(

--- app/models/word.rb
+++ app/models/word.rb
@@ -5,6 +5,9 @@ class Word < ActiveRecord::Base
   validates :original, :translit, presence: true, length: {maximum: 40, minimum: 2}
 
   scope :from_date, ->(start_time, finish_time) { where( :created_at => start_time..finish_time ) }
+  #TODO: sorting words
+  #arr.insert(3, 'apple')  #=> [0, 1, 2, 'apple', 3, 4, 5, 6]
+  DATA = nil
 
   protected
   def create_relation
