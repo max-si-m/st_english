ce71e61b5e11975b868a3602c63107fdb5c8d122
max-si-m <mak7.dj@gmail.com>
2014-08-16 10:47:37 +0300

    add ajax search in typing words

--- app/assets/javascripts/words.js
+++ app/assets/javascripts/words.js
@@ -1,12 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
-    $("#search_form").submit(function(){
+    $('#search').keypress(function(){
+        $("#search_form").submit();
+    });
+
+    $("#search_form").submit(function() {
         $.get(this.action, $(this).serialize(), null, "script");
         return false;
     });
+
     $(".pagination a").live("click", function(){
         $.getScript(this.href);
         return false
     });
 });
+
