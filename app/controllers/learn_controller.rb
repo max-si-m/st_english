eb3e1259488c2bdbbd2787530aa055e440b19298
Maxim Dzhuliy <mak7.dj@gmail.com>
2018-04-20 20:54:10 +0300

    Update learn_controller.rb

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -6,30 +6,29 @@ class LearnController < ApplicationController
   before_action :unknow_word, only: :unknow
 
 
-  def index
-  end
+  def index; end
 
-  def start
-  end
+  def start;end
 
   def know
     render "start"
   end
+  
   def unknow
     render "start"
   end
 
   protected
+  
   def get_words
     if (params.has_key?(:start_date) && params.has_key?(:finish_date) )
       @words = Word.from_date(params[:start_date], params[:finish_date])
-      if @words
-        redirect_to root_path, alert: "WORDS NOT FOUND!"
-      end
+      redirect_to root_path, alert: "WORDS NOT FOUND!" if @words
     else
       @words = Word.from_date(3.day.ago, Time.now)
     end
-    session[:way] = (params[:way] == "") ? "original": params[:way]
+    
+    session[:way] = params[:way].empty? ? "original": params[:way]
     set_tmp_data("learn_words", @words.to_a, Time.now + 1.days)
   end
 
@@ -41,7 +40,6 @@ class LearnController < ApplicationController
     else
       @word.word_statistic.increment_views
     end
-
   end
 
   def know_word
@@ -52,13 +50,13 @@ class LearnController < ApplicationController
 
   def unknow_word
     @word.word_statistic.increment_unknow
-    size = rand( 1..@words_arr.size )
+    size = rand(1..@words_arr.size)
     @words_arr.insert(size, @word)
     update_data
   end
 
   def update_data()
-    update_tmp_data("learn_words", @words_arr.to_a )
+    update_tmp_data("learn_words", @words_arr.to_a)
   end
 
 end
