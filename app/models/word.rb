c39fc95ed8c66e8bfd801a9a7e59f1b633c24693
Maxim Dzhuliy <mak7.dj@gmail.com>
2018-07-26 17:08:15 +0300

    Update word.rb
    fix codestyle

--- app/models/word.rb
+++ app/models/word.rb
@@ -16,7 +16,7 @@ class Word < ActiveRecord::Base
   validates :original, :translit, presence: true, length: {maximum: 40, minimum: 2}
 
   scope :from_date, ->(start_time, finish_time) { where( :created_at => start_time..finish_time ) }
-  default_scope ->{order("id DESC")}
+  default_scope     ->{ order('id DESC') }
   attr_accessor :data_array
 
   def self.search(search)
@@ -28,8 +28,8 @@ class Word < ActiveRecord::Base
   end
 
   protected
+  
   def create_relation
     WordStatistic.create(word: self)
   end
-
 end
